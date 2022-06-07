fdef1a4e6b4c454d0357c6331591bf243c0f9dc5
Bridgegrande-02 <bridgeatuhwera2014@gmail.com>
2022-06-07 03:28:04 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
-
 /**
- * main - print to screen
- *
- * Return: zero
- */
+   * main - print to screen
+   *
+   * Return: zero
+   */
 int main(void)
 {
-<<<<<<< HEAD
-puts("\"Programming is like building a multilingual puzzle");
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(Mystr);
 	return (0);
 }
-=======
-		puts("\"Programming is like building a multilingual puzzle");   
-		return (0);
-}	   
->>>>>>> 894c18773775818484a64dff442feaf0eccb2c71
